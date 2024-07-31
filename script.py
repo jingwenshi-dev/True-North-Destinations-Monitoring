3314f294d380c108740cfa44b8604f98c0db807d
Jingwen Shi <jingwensteven.shi@gmail.com>
2024-07-31 01:07:35 -0400

    Bug Fix: unknown status due to 2 same available and unavailable date dates on different month

--- script.py
+++ script.py
@@ -1,12 +1,9 @@
 import time
 import smtplib
 from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 
 RED = "\033[91m"
 GREEN = "\033[92m"
@@ -16,7 +13,7 @@ RESET = "\033[0m"
 def send_email():
     sender = "jingwensteven.shi@gmail.com"
     password = ""
-    recipients = [sender, 'huangdankun@gmail.com']
+    recipients = [sender]
 
     msg = MIMEText("https://www.truenorthdestinations.ca/book")
     msg['Subject'] = "泡泡屋有空房啦！"
@@ -30,43 +27,43 @@ def send_email():
     print("Message sent!")
 
 while True:
-    driver = webdriver.Chrome()
+    # driver = webdriver.Chrome()
+
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+
+    driver = webdriver.Chrome(options=options)
+
+    driver.get("https://www.truenorthdestinations.ca/book")
+    driver.implicitly_wait(3)
+
+    iframe = driver.find_elements(By.TAG_NAME, 'iframe')[0]
+    driver.switch_to.frame(iframe)
+    driver.implicitly_wait(3)
+
+    check_in = driver.find_element(By.NAME, "search_start_date")
+    check_in.click()
 
     try:
-        driver.get("https://www.truenorthdestinations.ca/book")
-        driver.implicitly_wait(3)
+        found = driver.find_element(By.XPATH, "//div[contains(@class, 'css-ocg6bg') and .//div[contains(@class, '2024-08')] and .//div[@class='day default']/p[text()='29'] and .//div[@class='day default']/div[@class='dot']]")
+    except NoSuchElementException:
+        found = None
+    try:
+        not_found = driver.find_element(By.XPATH, "//div[contains(@class, 'css-ocg6bg') and .//div[contains(@class, '2024-08')] and .//div[@class='day default disabled']/p[text()='29']]")
+    except NoSuchElementException:
+        not_found = None
 
-        iframe = driver.find_elements(By.TAG_NAME, 'iframe')[0]
-        driver.switch_to.frame(iframe)
-        driver.implicitly_wait(3)
+    if not_found and not found:
+        print(f"{RED}ROOM NOT AVAILABLE{RESET}")
+    elif found and not not_found:
+        print(f"{GREEN}ROOM AVAILABLE{RESET}")
+        send_email()
+    else:
+        print("found", found)
+        print("not_found", not_found)
+        print(f"{YELLOW}STATUS UNKNOWN{RESET}")
 
-        check_in = driver.find_element(By.NAME, "search_start_date")
-        check_in.click()
+    driver.quit()
 
-        try:
-            not_found = WebDriverWait(driver, 3).until(
-                EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'day') and contains(@class, 'default') and contains(@class, 'disabled')]/p[text()='29']"))
-            )
-        except TimeoutException:
-            not_found = False
-
-        try:
-            found = WebDriverWait(driver, 3).until(
-                EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'day') and contains(@class, 'default')]/p[text()='29']/following-sibling::div[contains(@class, 'dot')]"))
-            )
-        except TimeoutException:
-            found = False
-
-        if not_found and not found:
-            print(f"{RED}ROOM NOT AVAILABLE{RESET}")
-        elif found and not not_found:
-            print(f"{GREEN}ROOM AVAILABLE{RESET}")
-            send_email()
-        else:
-            print(f"{YELLOW}STATUS UNKNOWN{RESET}")
-
-    finally:
-        driver.quit()
-
-    # Run every 5 minutes
-    time.sleep(290)
+    # Run every 10 minutes
+    time.sleep(600)
